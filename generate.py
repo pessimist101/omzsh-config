5e3040984826b9b16021bd7ae58a203d2bfd075c
pessimist101 <issythegirl@gmail.com>
2020-07-26 19:39:52 +0100

    feat/Added generate script  - pulls from local profile .zshrc  - strips comment lines  - makes copy in repo

---
+++ generate.py
@@ -0,0 +1,22 @@
+with open('/home/issy/.zshrc','r') as f:
+    f = f.read()
+
+config = []
+
+def ignore_line(line):
+    return ((line.startswith('#'))and (line != '# Aliases')) or (line == '')
+
+for line in f.split('\n'):
+    if ignore_line(line):
+        continue
+    else:
+        config.append(line)
+
+config = '\n'.join(config)
+
+with open('.new-zshrc','w') as f:
+    f.write(config)
+    print('Written config to file!')
+
+print('')
+print(config)
